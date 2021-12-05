47c9cb16bdf41c6270aba6056844f8c038630c26
Cifelse <lemsicode@gmail.com>
2021-12-05 20:04:56 +0800

    Deleted node_modules and ENV file. Added Database.js

---
+++ database.js
@@ -0,0 +1,22 @@
+const { createPool } = require('mysql');
+
+const pool = createPool ({
+    host: "localhost",
+    port: "3310",
+    user: "root",
+    password: "12345",
+    database: "imdb_ijs",
+    connectionLimit: 10
+})
+
+// Enter Query Here
+pool.query(`SELECT * FROM movies WHERE id < 99`,
+    
+    (err, result, fields) => {
+        if (err) {
+            return console.log(err);
+        }
+        return console.log(result);
+})
+
+module.exports = pool;
